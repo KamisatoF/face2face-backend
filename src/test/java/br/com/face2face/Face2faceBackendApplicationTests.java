5a72b7de9e6aa225eb242c1d629e7c386d57abd7
Fabio Kamisato <fabiokamisato@gmail.com>
2022-07-09 09:58:38 -0300

    first commit

---
+++ src/test/java/br/com/face2face/Face2faceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.face2face;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Face2faceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
