709aa8649b1c53e86a0e5a04c25352de37579630
Fabio Kamisato <fabiokamisato@gmail.com>
2022-08-08 18:28:24 -0300

    Generate Fake Data

--- src/main/java/br/com/face2face/utils/GenerateFakeData.java
+++ src/main/java/br/com/face2face/utils/GenerateFakeData.java
@@ -14,18 +14,20 @@ import br.com.face2face.repository.ServicoRepository;
 
 @Service
 public class GenerateFakeData {
-	
+
 	@Autowired
 	ServicoRepository repo;
-	
+
 	@PostConstruct
 	public void setUp() {
 		List<Servico> list = new ArrayList<>();
-		list.add(new Servico(null, "nom1", "det1", BigDecimal.TEN));
-		list.add(new Servico(null, "nom2", "det2", BigDecimal.TEN));
-		list.add(new Servico(null, "nom3", "det3", BigDecimal.TEN));
-		list.add(new Servico(null, "nom4", "det4", BigDecimal.TEN));
-		
+		list.add(new Servico(null, "Brunch completo",
+				"O brunch completo serve até 10 pessoaos com frios e queijos, diferentes tipos de pães, patês e geleias, iogurte, bolos, pão de queijo e frutas.",
+				new BigDecimal("350.00")));
+		list.add(new Servico(null, "Snacks", "Os snacks servem até 10 pessoas e são compostos das seguintes opções: biscoitos doces e salgados, pão de queijo e salada de frutas", new BigDecimal("100.00")));
+		list.add(new Servico(null, "Água gelada", "Água gelada dísponivel para até 10 pessoas", new  BigDecimal("50.00")));
+		list.add(new Servico(null, "Material de escritório", "10 blocos para anotação e canetas", new BigDecimal("100.00")));
+
 		repo.saveAll(list);
 	}
 
