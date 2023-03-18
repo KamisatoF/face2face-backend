21ba11cbfda9cb46a05eb7a233e905f4d9c89126
Fabio Kamisato <fabiokamisato@gmail.com>
2023-03-18 02:56:33 -0300

    Incluindo validação CadastroService

--- src/main/java/br/com/face2face/service/CadastroService.java
+++ src/main/java/br/com/face2face/service/CadastroService.java
@@ -33,6 +33,11 @@ public class CadastroService {
     }
 
     private ServiceResponse validate(Usuario usuario) {
+        if (usuario.getSenha() == null || usuario.getSenha().equals("") || usuario.getCpf() == null || usuario.getCpf().equals("") ||
+                usuario.getTelefone() == null || usuario.getTelefone().equals("") || usuario.getEmail() == null || usuario.getEmail().equals("") ||
+                usuario.getNome() == null || usuario.getNome().equals("")) {
+            return new ServiceResponse(HttpStatus.BAD_REQUEST, "Por favor, preencha todos os campos!", null);
+        }
         if (usuario.getCpf().length() != 11) {
             return new ServiceResponse(HttpStatus.BAD_REQUEST, "O CPF deve ter 11 digitos!", null);
         } else if (usuario.getTelefone().length() != 11) {
